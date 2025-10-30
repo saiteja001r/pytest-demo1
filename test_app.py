b62fd7ae3c0812017c818bcd6d82075014178da6
saiteja001 <saiteja001r@gmail.com>
2025-10-30 10:47:43 -0700

    added test files

---
+++ test_app.py
@@ -0,0 +1,19 @@
+import pytest 
+from app import add, multipy, divide
+
+def test_add():
+    assert add(3, 5) == 8
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+def test_multiply():
+    assert multipy(3, 5) == 15
+    assert multipy(2, 0) == 0
+
+def test_divide():
+    assert divide(10, 2) == 5
+    with pytest.raises(ValueError):
+        divide(5, 0)
+
+def test_fail():
+    assert add(2,2) == 5
